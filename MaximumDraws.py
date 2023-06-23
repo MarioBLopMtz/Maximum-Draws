e192c0073c8e9429d59e42ad2913eb86b91fff75
MarioBLopMtz <137469342+MarioBLopMtz@users.noreply.github.com>
2023-06-23 00:19:09 -0600

    Update MaximumDraws.py

--- MaximumDraws.py
+++ MaximumDraws.py
@@ -12,7 +12,7 @@ def maximumDraws(n): #Recibe como parametro la variable n
 if __name__ == '__main__': #Ejecuta el script directamente 
     fptr = open(os.environ['OUTPUT_PATH'], 'w') #Abre el archivo que le asigna la pagina Hackerrank para darle los valores de entrada.
 
-    t = int(input().strip()) #Lee el numero de colores de calcetines.
+    t = int(input().strip()) #Lee el numero de de casos prueba.
 
     for t_itr in range(t): #Ciclo for para iterar sobre los casos de prueba que esta dentro del rango de la variable t.
         n = int(input().strip()) #Lee el numero de colores de calcetines (n) para el caso de prueba actual.
