dcbe6dde22b9f6d4332093afba8fe54248edf396
MarioBLopMtz <137469342+MarioBLopMtz@users.noreply.github.com>
2023-06-22 23:21:38 -0600

    Update MaximumDraws.py

--- MaximumDraws.py
+++ MaximumDraws.py
@@ -1,13 +1,13 @@
 #!/bin/python3
-
+#Importamos librerias
 import math
 import os
 import random
 import re
 import sys
-
-def maximumDraws(n):
-    return n + 1
+#Declaramos la funcion.
+def maximumDraws(n): #Recibe como parametro la variable n
+    return n + 1 #Devuelve n + 1 ya que 
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
