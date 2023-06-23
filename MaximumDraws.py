0d72207abac1f13d7a994116eb12781cc5fbc62e
MarioBLopMtz <137469342+MarioBLopMtz@users.noreply.github.com>
2023-06-22 23:40:40 -0600

    Update MaximumDraws.py

--- MaximumDraws.py
+++ MaximumDraws.py
@@ -7,10 +7,10 @@ import re
 import sys
 #Declaramos la funcion.
 def maximumDraws(n): #Recibe como parametro la variable n
-    return n + 1 #Devuelve n + 1 ya que 
+    return n + 1 #Devuelve n + 1 ya que si sacamos un calcetin de un color, aun debemos sacar otro para que sea el par completo.
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+if __name__ == '__main__': #Ejecuta el script directamente 
+    fptr = open(os.environ['OUTPUT_PATH'], 'w') #Abre el archivo que le asigna la pagina Hackerrank para darle los valores de entrada.
 
     t = int(input().strip())
 
